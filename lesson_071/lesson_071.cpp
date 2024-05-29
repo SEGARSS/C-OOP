06169270494a5b9e5fcf4a22dcc9205993779c04
SEGARSS <vim_branet@mail.ru>
2024-05-29 17:53:12 +0500

    ооп

--- lesson_071/lesson_071.cpp
+++ lesson_071/lesson_071.cpp
@@ -6,6 +6,24 @@ using namespace std;
 
 
 //-------------------------------------------------------------------------------------------------
+class Timer
+{
+public:
+	Timer()
+	{
+		start = chrono::high_resolution_clock::now();
+	}
+	~Timer()
+	{
+		end = chrono::high_resolution_clock::now();
+		chrono::duration<float> duration = end - start;
+		cout << "DURATION " << duration.count() << " s" << endl;
+	}
+
+private:
+	chrono::time_point<chrono::steady_clock> start, end;
+};
+//-------------------------------------------------------------------------------------------------
 int Sum(int a, int b)
 {
 	this_thread::sleep_for(chrono::milliseconds(2000));
@@ -19,6 +37,8 @@ int Sum(int a, int b)
 //-------------------------------------------------------------------------------------------------
 int main()
 {
+	Timer st;
+
 	setlocale(LC_ALL,"ru");
 
 	int result;
@@ -27,7 +47,7 @@ int main()
 	{ 
 		result = Sum(2, 5);
 	});
-
+	
 	for (size_t i = 1; i <= 10; i++)
 	{
 		cout << "ID потокa = " << this_thread::get_id() << "\tmain works\t" << endl;
@@ -40,3 +60,5 @@ int main()
 	return 0;
 }
 //-------------------------------------------------------------------------------------------------
+
+/*Реализация как посмотреть сколько времени тратит та или инная операция, или участок кода.*/
