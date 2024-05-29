ad0d070fd42dd2c58403b2bf501268a72a028ff0
SEGARSS <vim_branet@mail.ru>
2024-05-29 09:55:57 +0500

    ооп

---
+++ lesson_070/lesson_070.cpp
@@ -0,0 +1,12 @@
+// Захват контекста лямбдой | Лямбда функции | Лямбда-выражения | Анонимные функции | Урок #144 - Видео №94.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL,"ru");
+
+
+
+	return 0;
+}
