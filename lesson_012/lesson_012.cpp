60e044430c6620eef44ee50ab5e0351f33275c5f
SEGARSS <vim_branet@mail.ru>
2024-11-06 15:45:37 +0500

    ООП

--- lesson_012/lesson_012.cpp
+++ lesson_012/lesson_012.cpp
@@ -3,35 +3,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
+//----------------------------------------------------------------------------------
 class point
 {
-	int x;
-	int y;
-
 public:
 	point()
 	{
 		x = 0;
 		y = 0;
-		cout << this << " Constructor" << endl;
-
 	}
 	point(int valueX, int valueY)
 	{
 		x = valueX;
 		y = valueY;
-		cout << this << " Constructor" << endl;
-	}
-
-	bool operator == (const point& other)
-	{
-		return this->x == other.x && this->y == other.y;
-	}
-
-	bool operator != (const point& other)
-	{
-		return !(this->x == other.x && this->y == other.y);
 	}
 
 	point operator +(const point& other)
@@ -40,41 +24,21 @@ public:
 		temp.x = this->x + other.x;
 		temp.y = this->y + other.y;
 		return temp;
-		
-	}
-	
-
-	int GetX()
-	{
-		return x;
-	}
-
-	void SetX(int valueX)
-	{
-		x = valueX;
-	}
-
-	int GetY()
-	{
-		return y;
-	}
-
-	void SetY(int y)
-	{
-		this->y = y;
 	}
 
 	void Print()
 	{
 		cout << "X = " << x << "\t Y = " << y << endl << endl;
 	}
-};
 
+private:
+	int x;
+	int y;
+};
+//----------------------------------------------------------------------------------
 int main()
 {
-	setlocale(LC_ALL, "ru");
-
-	
+	setlocale(LC_ALL, "ru");	
 
 	point a(5, 85);
 	point b(3, 32);
@@ -85,3 +49,4 @@ int main()
 
 	return 0;
 }
+//----------------------------------------------------------------------------------
--- lesson_013/lesson_013.cpp
+++ lesson_013/lesson_013.cpp
@@ -4,117 +4,73 @@
 #include<string>
 using namespace std;
 
+
+//----------------------------------------------------------------------------------
 class point
 {
-	int x;
-	int y;
 public:
-
 	point()
 	{
 		x = 0;
 		y = 0;
-		cout << this << "Constructor" << endl;
 	}
+
 	point(int valueX, int valueY)
 	{
 		x = valueX;
 		y = valueY;
-		cout << this << "Constructor" << endl;
 	}
 
-	bool operator == (const point& other)
-	{
-		return this->x == other.x && this->y == other.y;
-	}
-
-	bool operator != (const point& other)
-	{
-		return !(this->x == other.x && this->y == other.y);
-	}
-
-	point operator +(const point& other)
-	{
-		point temp;
-		temp.x = this->x + other.x;
-		temp.y = this->y + other.y;
-		return temp;
-	}
-
-	int GetX()
-	{
-		return x;
-	}
-
-	void SetX(int valueX)
-	{
-		x = valueX;
-	}
-
-	int GetY()
-	{
-		return y;
-	}
-
-	int SetY()
-	{
-		this->y = y;
-	}
 	void Print()
 	{
 		cout << "X = " << x << "\t Y = " << y << endl << endl;
 	}
 
-	point& operator ++(int value) // Инкремент
+	point &operator ++(int value) // Инкремент
 	{
 		point temp(*this);
-		this->x++;
-		this->y++;
+		this -> x++;
+		this -> y++;
 		return temp;
 	}
-
-	point& operator ++ () // Дикремент
+	point &operator ++ ()//Запись реализации другая, но вывод тотже.
 	{
-		//Записи реализации разные, но они вывод делают одинаковый. Просто для понимания как можно написать данный код.
-		this->x++;
-		this->y += 1;
-
+		this -> x++;
+		this -> y += 1;
 		return *this;
 	}
 
-	point& operator --(int value) // Инкремент
+	point &operator --(int value) // Дикремент
 	{
 		point temp(*this);
-		this->x--;
-		this->y--;
+		this -> x--;
+		this -> y--;
 		return temp;
 	}
-
-	point& operator -- () // Дикремент
+	point &operator -- ()//Запись реализации другая, но вывод тотже.
 	{
-		//Записи реализации разные, но они вывод делают одинаковый. Просто для понимания как можно написать данный код.
-		this->x--;
-		this->y -= 1;
-
+		this ->x -= 1;
+		this ->y --;
 		return *this;
 	}
+
+private:
+	int x;
+	int y;
 };
-
+//----------------------------------------------------------------------------------
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
 	point a(1, 1);
-
 	point b = ++a;
-
 	a.Print();
 
 	point c(1, 1);
-
 	point v = --c;
-
 	c.Print();
 
 	return 0;
 }
+//----------------------------------------------------------------------------------
